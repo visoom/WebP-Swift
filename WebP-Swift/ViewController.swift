857f1f4565200af37c84bf44e49a0c22c7a356e8
Marco <nogard20@gmail.com>
2016-10-09 22:51:30 +0200

    Initial Commit

---
+++ WebP-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebP-Swift
+//
+//  Created by Mark on 09/10/2016.
+//  Copyright © 2016 Visoom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
